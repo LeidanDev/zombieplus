4536583c551d5ce75525ba247e28d9e55c57a0be
unknown <dancardosogaldino@gmail.com>
2026-06-08 20:43:09 -0300

    atualizando novos casos de testes

--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,20 +1,32 @@
-const {test} = require('../support')
+const { test } = require('../support')
 
 const data = require('../support/fixtures/movies.json')
 
-const {executeSql} = require('../support/database')
+const { executeSql } = require('../support/database')
 
 
-test ('deve poder cadastrar um novo filme', async ({page}) => {
+test('deve poder cadastrar um novo filme', async ({ page }) => {
 
     const movie = data.create
+
     await executeSql(`DELETE from movies WHERE title = '${movie.title}';`)
 
-    await page.login.visit()
-    await page.login.submit('admin@zombieplus.com', 'pwd123')
-    await page.movies.isLoggedIn()
-
-    await page.movies.create(movie.title, movie.overview, movie.company, movie.release_year)
-
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.create(movie)
     await page.toast.containText('Cadastro realizado com sucesso!')
+
+})
+
+test('não deve cadastrar quando os campos obrigatorios não são preenchidos', async ({ page }) => {
+    
+    await page.login.do('admin@zombieplus.com', 'pwd123', 'Admin')
+    await page.movies.goForm()
+    await page.movies.submit()
+
+    await page.movies.alertHaveText([
+        'Por favor, informe o título.',
+        'Por favor, informe a sinopse.',
+        'Por favor, informe a empresa distribuidora.',
+        'Por favor, informe o ano de lançamento.'
+    ])
 })
